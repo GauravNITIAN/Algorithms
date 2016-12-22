f74f82ef09175dc6551e3c2309b127160909e3cb
Gaurav <gaurav4ever98@gmail.com>
2016-12-23 00:14:37 +0530

    Update BesttimeTobuySellStock.cpp

--- Dynamic programing/BesttimeTobuySellStock.cpp
+++ Dynamic programing/BesttimeTobuySellStock.cpp
@@ -1,19 +1,28 @@
 #include<iostream>
 
 using namespace std;
+/*
+
+The Problem is sovle the best time to buy a stock and sell at the best time so that it gains maximum profilt
+
+Idea : - the idea is to traverse the array choose the first element as to buy then check to next if it is
+         greater than the previous then we get the profit otherwise if the next day has smaller stock value
+         then we make that day as buy day and then traverse other in the same way. MeanWhile we update each profit
+         with the max profit each day.
+*/
 
 int BestTimeToBuyStock(int ar[],int n)
 {
-   int buy = ar[0];
+   int buy = ar[0];              // making the first element as buy
    int profit = 0,max_profit = 0;	
    for(int i=1;i<n;i++)
    {
-      if(buy <= ar[i])
+      if(buy <= ar[i])        // if the next element is smaller then we have a profit 
         profit = ar[i]-buy;
       else
-      buy = ar[i];  
+      buy = ar[i];  // if next day is smaller value stock  then we buy the stock this day and sell it to another day
       
-      if(profit > max_profit)
+      if(profit > max_profit)        // update the max_profit each time
       max_profit = profit;
    }
    
